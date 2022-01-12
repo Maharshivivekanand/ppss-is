0252767c1d66c86dd7717fd5690d7bcc468676b1
Vivekanand Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-01-12 05:44:23 +0000

    p1original.c done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,13 @@
+/*Add Two Numbers*/
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  float sum;
+  printf("enter two numbers\n");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("addition of %d + %d is= %f\n",a,b,sum);
+  return(0);
 
 }
