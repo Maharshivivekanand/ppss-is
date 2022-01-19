e880afcc6a5d86e2994f312813d55a86d947c05b
Vivekanand Shridhar Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-01-19 03:58:55 +0000

    p3 done

--- p3original.c
+++ p3original.c
@@ -2,28 +2,28 @@
 
 void input(int a,int b)
 {
-  int shri
-  
+  int shri;
+  printf("enter a value\n");
   scanf("%d", &shri);
 }
 
-void add(int a,int b,int *sum)
+void add(int a,int b,sum)
 {
-  int sum;
-  sum=a+b;
-  return sum;
+  int *sum;
+  *sum=a+b;
+  return *sum;
 
 }
 void output(int a,int b,int sum)
 {
-  printf("addition of %d + %d is=%d\n",a,b,sum);
+  printf("addition of %d + %d is=%d\n",a,b,*sum);
 
 }
- int input()
+ void input()
  {
    int g,h,sum;
    g=input(int a,int b);
    h=input(int a,int b);
    sum=add(g,h);
-   output=(g,h,sum;
+   output=(g,h,sum);
  }
